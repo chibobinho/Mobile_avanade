d14c73b1b61a85d2bfe8a6a5960f9cda582ddc0e
Gustavo Henrique <82419986+GustavoHenriqueFerreira@users.noreply.github.com>
2022-03-28 00:16:05 -0300

    Update ponto.js

--- Avanade_mobile/src/screens/ponto.js
+++ Avanade_mobile/src/screens/ponto.js
@@ -5,54 +5,52 @@ import {
   View,
   Image,
   TouchableOpacity,
+  ImageBackground,
 } from 'react-native';
 
 class Ponto extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      Email: '',
-      base64: '',
-    };
-  }
 
   render() {
     return (
       <View style={styles.main}>
-        <Image style={styles.imgPonto} source={require('../../assets/img/mapa.png')} />
-        <View style={styles.espacoTitulo}>
-          <Text style={styles.tituloBicicletario}>Bike Runners - Bike Shop</Text>
+        <ImageBackground style={[styles.imgPonto]} source={require('../../assets/img/mapa.png')}>
+          <Image style={styles.mainBtnVoltar} source={require('../../assets/img/Icone_voltar.png')} />
+        </ImageBackground>
+
+        <View style={styles.mainBody}>
+          <View style={styles.espacoTitulo}>
+            <Text style={styles.tituloBicicletario}>Bike Runners - Bike Shop</Text>
+          </View>
+          <View style={styles.infoBicicletario}>
+            <View>
+              <Text style={styles.tituloInfo}>Endereço:</Text>
+              <Text style={styles.textoInfo}>R. Emília Marengo, 320 - Tatuape, São Paulo - SP, 03336-000</Text>
+            </View>
+
+            <View>
+              <Text style={styles.tituloInfo}>Áreas atendidas:</Text>
+              <Text style={styles.textoInfo}>São Paulo</Text>
+            </View>
+
+            <View>
+              <Text style={styles.tituloInfo}>Horas:</Text>
+              <Text style={styles.textoInfo}>Aberto ⋅ Fecha às 19:00</Text>
+            </View>
+
+            <View>
+              <Text style={styles.tituloInfo}>Vagas:</Text>
+              <Text style={styles.textoInfo}>Disponiveis = 7</Text>
+              <Text style={styles.textoInfo}>Totais = 15</Text>
+            </View>
+
+            <View style={styles.btnPosicionamento}>
+              <TouchableOpacity onPress={this.realizarLogin} style={styles.btnPonto}>
+                <Text style={styles.cardPontosText}>Estou no ponto</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-
-        <View style={styles.infoBicicletario}>
-          <View>
-            <Text style={styles.tituloInfo}>Endereço:</Text>
-            <Text style={styles.textoInfo}>R. Emília Marengo, 320 - Tatuape, São Paulo - SP, 03336-000</Text>
-          </View>
-
-          <View>
-            <Text style={styles.tituloInfo}>Áreas atendidas:</Text>
-            <Text style={styles.textoInfo}>São Paulo</Text>
-          </View>
-
-          <View>
-            <Text style={styles.tituloInfo}>Horas:</Text>
-            <Text style={styles.textoInfo}>Aberto ⋅ Fecha às 19:00</Text>
-          </View>
-
-          <View>
-            <Text style={styles.tituloInfo}>Vagas:</Text>
-            <Text style={styles.textoInfo}>Disponiveis = 7</Text>
-            <Text style={styles.textoInfo}>Totais = 15</Text>
-          </View>
-
-          <View style={styles.btnPosicionamento}>
-            <TouchableOpacity onPress={this.realizarLogin} style={styles.btnPonto}>
-              <Text style={styles.cardPontosText}>Estou no ponto</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
+      </View >
     );
   }
 }
@@ -64,8 +62,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#CECED7',
   },
 
+  mainBtnVoltar: {
+    width: 20,
+    height: 20,
+    marginLeft: 25,
+    marginTop: 10
+  },
+
   imgPonto: {
-    height: 225,
+    height: 270,
+  },
+
+  mainBody: {
+    flex: 1,
+    justifyContent: 'space-between',
   },
 
   espacoTitulo: {
@@ -73,12 +83,14 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     alignItems: 'center',
     justifyContent: 'center',
-    /* shadowOffset: { width: 4, height: 4, },
-    shadowColor: 'black',
-    shadowOpacity: 1.0, */
+    shadowColor: '#rgba(0, 0, 0, 0.4)',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 1,
+    shadowRadius: 4,
   },
 
   tituloBicicletario: {
+    //fontFamily: '',
     fontSize: 30,
     fontWeight: 'bold',
     color: '#000'
@@ -87,14 +99,20 @@ const styles = StyleSheet.create({
   infoBicicletario: {
     paddingLeft: 38,
     paddingRight: 38,
+    flex: 1,
+    justifyContent: 'space-evenly',
+    paddingBottom: 10,
+    paddingTop: 10,
   },
 
   tituloInfo: {
+    //fontFamily: '',
     fontSize: 25,
     color: '#000',
   },
 
   textoInfo: {
+    //fontFamily: '',
     fontSize: 18,
     color: '#000',
   },
@@ -114,9 +132,11 @@ const styles = StyleSheet.create({
   },
 
   cardPontosText: {
-    fontSize: 16,
+    //fontFamily: '',
+    fontSize: 18,
     fontWeight: 'bold',
     color: '#000',
+    textAlign: 'center',
   },
 });
 
